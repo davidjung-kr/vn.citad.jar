f74e6067fdc5837c1cfae31731c5512ee6b267dc
davidjung-kr <jerryjueng@daum.net>
2020-12-07 23:32:59 +0900

    first commit

---
+++ java/vn.citad/src/vn/citad/gaw/Otf.java
@@ -0,0 +1,12 @@
+package vn.citad.gaw;
+/**
+ * [ VIETNAM CITAD ] Otf
+ * 	- Out-going
+ * ※ By David Jung
+ * 
+ * ※ License: LGPL-v2.1 (https://www.gnu.org/licenses/old-licenses/lgpl-2.1.html)
+ * ※ See: https://github.com/davidjung-kr/vn.citad.jar
+ */
+public class Otf {
+
+}
