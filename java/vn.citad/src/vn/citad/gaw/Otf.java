44b3c3e43df5cd93c10c3470cf706485a023cdef
davidjung-kr <jerryjueng@daum.net>
2020-12-08 22:45:30 +0900

    데이터클래스 첫 정의, 필드 클래스 정의 추가

--- java/vn.citad/src/vn/citad/gaw/Otf.java
+++ java/vn.citad/src/vn/citad/gaw/Otf.java
@@ -8,5 +8,15 @@ package vn.citad.gaw;
  * ※ See: https://github.com/davidjung-kr/vn.citad.jar
  */
 public class Otf {
+	private Field[] fields = { new Field("REC_TYPE", 2) };
+}
 
+class Field {
+	private int length;
+	private String name;
+	public Field(String name, int length) {
+		this.name = name;
+		this.length = length;
+	}
+	
 }
