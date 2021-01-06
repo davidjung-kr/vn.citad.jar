b577aec27d3ca65848e3e4ed0ef700358c542188
davidjung-kr <jerryjueng@daum.net>
2021-01-06 14:40:33 +0900

    패딩 타입 분리 => vn.citad.type.Padding.java

--- java/vn.citad/src/vn/citad/type/OtfHeader.java
+++ java/vn.citad/src/vn/citad/type/OtfHeader.java
@@ -1,92 +1,33 @@
 package vn.citad.type;
 
-
-
-public class OtfHeader {
-	public enum PADDING {
-		NONE,
-		LEFT,
-		RIGHT
-	}
-	
-	public static final int length = 5;
-	public String[] fields = new String[length];
-	private final String[] fieldNames = {
-		"REC_TYPE",
-		"CI_CODE",
-		"FILE_NAME",
-		"TR_DATE",
-		"DATA_CNT"
-	};
-	
-	public final PADDING[] fieldPadding = {
-			PADDING.NONE,
-			PADDING.LEFT,
-			PADDING.RIGHT,
-			PADDING.NONE,
-			PADDING.NONE
-	};
-	
-	private final int[] fieldSizes = {
-		2, 12, 25, 8, 8
-	};
-	
-	private final boolean[] fieldMandatory = {
-		true,
-		true,
-		true,
-		true,
-		true		
-	};
-	
-	public void setFields(String[] array) {
-		this.fields = array;
-	}
-	
-	public void setField(int index, String value) {
-		this.fields[index] = value;
-	}
-	
-	public int getFieldIndex(String fieldName) {
-		int result = -1;
-		for(int i=0; i<fieldNames.length; i++) {
-			if(fieldNames[i].equals(fieldName)) {
-				result = i;
-				break;
+public class OtfHeader extends Record {
+	public OtfHeader() {
+		super(
+			5,
+			new String[]{
+				"REC_TYPE",
+				"CI_CODE",
+				"FILE_NAME",
+				"TR_DATE",
+				"DATA_CNT"
+			},
+			new int[] {
+				2, 12, 25, 8, 8
+			},
+			new boolean[]{
+				true,
+				true,
+				true,
+				true,
+				true		
+			},
+			new Padding[] {
+				Padding.NONE,
+				Padding.LEFT,
+				Padding.RIGHT,
+				Padding.NONE,
+				Padding.NONE	
 			}
-		}
-		return result;
-	}
-	
-	public String getFieldName(int index) {
-		return this.fieldNames[index];
-	}
-	
-	public int getFieldSize(int index) {
-		return this.fieldSizes[index];
-	}
-	
-	public int getFieldSize(String fieldName) {
-		int index = -1;
-		for(int i=0; i<fieldNames.length; i++) {
-			if(fieldName.equals(fieldNames[i]) ) {
-				index = i;
-				break;
-			}
-		}
-		return fieldSizes[index];
-	}
-	
-	public boolean isFieldMandatory(int index) {
-		return this.fieldMandatory[index];
-	}
-	
-	public boolean isFieldMandatory(String fieldName) {
-		int index = this.getFieldIndex(fieldName);
-		return index>=0 ? fieldMandatory[index]:false; // -1일 때 무조건 false
-	}
-	
-	public String getFieldValue(int index) {
-		return this.fields[index];
+		);
 	}
 }
